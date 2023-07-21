e90b12c4a528b0338ecac97d7f74bb56d424edf2
Axel Pröger <a.proeger@gmail.com>
2023-07-21 19:25:23 +0200

    first commit

---
+++ app.config.ts
@@ -0,0 +1,24 @@
+export default defineAppConfig({
+  docus: {
+    title: 'BK Guide',
+    socials: {
+      twitter: 'Beyond_Kick',
+      instagram: 'beyondkickboxing',
+    },
+    aside: {
+      level: 0,
+      collapsed: false,
+      exclude: []
+    },
+    main: {
+      padded: true,
+      fluid: false
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: [],
+      fluid: false
+    }
+  }
+})
---
+++ components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+  Beyond Kickboxing Guide
+</template>
